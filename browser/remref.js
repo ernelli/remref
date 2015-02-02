88c0269c90345e48056072a5e47aea363cd27af0
Mattias Ernelli <mattias@ernelli.com>
2015-02-02 15:31:53 +0100

    Added reflection API Added tests for reflection API * reflection API is work in progress, currently not working

--- browser/remref.js
+++ browser/remref.js
@@ -1,6 +1,12 @@
 var serverURL = "http://localhost:8080";
 
-function RemoteRequest(id, options) {
+if(typeof toArray === "undefined") {
+    function toArray(a) {
+        return Array.prototype.slice.call(a);
+    }
+}
+
+function RemoteRequest(clientid, options) {
     var txid = 1;
     var clientid;
     var requests = {};
@@ -21,17 +27,17 @@ function RemoteRequest(id, options) {
         }
     });
     
-    socket.on('request', function(message) {
-        var _txid = message.txid, method = message.method;
+    socket.on('request', function(request) {
+        var _txid = request.txid;
         if(this.onrequest) {
-            console.log(clientid + ": process request: ", message);
-            this.onrequest(message, function(reply) {
+            console.log(clientid + ": process request: ", request);
+            this.onrequest(request.message, function(reply) {
                 console.log(clientid + ": request processed, send reply: ", reply);
                 socket.emit('reply', { txid: _txid, message: reply });
             });
         } else {
             console.log(clientid + ": request handler not present on: ", this);
-            socket.emit('reply', { txid: message.txid });
+            socket.emit('reply', { txid: request.txid });
         }
     }.bind(this));
 
@@ -77,20 +83,103 @@ function RemoteRequest(id, options) {
             delete this.readycb;
         } 
     }.bind(this));
-    socket.emit('init', { clientid: id });
+    socket.emit('init', { clientid: clientid });
 }
 
+function RemoteReflection(clientid, remoteid, options) {
+    var rpc = new RemoteRequest(clientid, options);    
 
+    // id -> object
+    var id=1, local = {};
 
-
-
-if(typeof toArray === "undefined") {
-    function toArray(a) {
-        return Array.prototype.slice.call(a);
+    function invoke(item, property, params) {
+        var e = local[item];
+        if(e) {
+            console.log("apply: " + property + ", on object: ", e);
+            return e[property].apply(e, params);
+        }
     }
+
+    function reflect(item, options) {
+        var i, o, p, res;
+
+        if(typeof item == "string") {
+            o = window[item];
+        } else if(Array.isArray(item) ) {
+            o = window;
+            for(i = 0; i < item.length; i++) {
+                o = o.item[i];
+            }
+        }
+
+        console.log("reflect item: ", item, ", is object: ", o);
+
+        local[id] = o;
+
+        res = { id: id, map: {} };
+
+        for(p in o) {
+            console.log("map property: " + p);
+            if(typeof o[p] === "function") {
+                res.map[p] = 1;
+            } else {
+                res.map[p] = 2;
+            }
+        }
+
+        return res;
+    }
+
+    rpc.onrequest = function(message, cb) {
+        var method = message.method;
+        var params = message.params;
+
+        console.log("reflect rpc, got message: ", message);
+        console.log("reflect rpc, method: ", method);
+
+        if(method === "invoke") {
+            var res = invoke(params.item, params.propery, params.params);
+            cb(res);
+        } else if(method === "reflect") {
+            console.log("got reflect request: ", message);
+            var res = reflect(params.item, params.options);
+            cb(res);
+        } else {
+            cb( { error: "invalid method: " + message.method } );
+        }
+    }
+
+    this.reflect = function(item, options) {
+        var p, map, res = rpc.request(remoteid, { method: "reflect", params: { item: item, options: options  } });
+
+        map = res.map;
+
+        console.log("got reflect map: ", map);
+
+        for(p in map) {
+            if(map[p] === 1) { // reflected function
+                //console.log("replace stub with rpc call: to ", res.id, ", method: ", p);
+                map[p] = (function(item, prop) {
+                    return function() {
+                        rpc.request(remoteid, { method: 'invoke', params: { item: item, property: prop, params: toArray(arguments) }});
+                    }
+                })(res.id, p);
+            }
+        }
+        return map;
+    }
+
 }
 
-var rpc = {
+
+
+
+
+/*
+
+
+
+{
     invoke: function (o, m, args) {
         var params = [o,m].concat(toArray(args));
         console.log("params: " + params);
@@ -165,3 +254,4 @@ function runTests(o) {
 }
 
 runTests(local);
+*/
